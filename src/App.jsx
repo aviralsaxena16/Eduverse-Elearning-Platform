bc3bb5e24cf40f5b73efc348438e549cb5470cf0
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-01-05 19:40:06 +0530

    Updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import LandingPage from './components/landingPage/LandingPage';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Navbar from './components/landingPage/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}></Route>
+    </Routes>
+    </BrowserRouter>
+      
   )
 }
 
