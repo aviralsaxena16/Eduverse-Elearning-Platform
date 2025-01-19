97ecbcc734c1d7fcdb65bfe908a1b4cf5215e39e
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-01-19 19:00:22 +0530

    Routes simplified

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,47 +1,31 @@
-import {useContext} from 'react'
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from '../pages/LandingPage/LandingPage.jsx'
-import MainPage from '../pages/MainPage/MainPage.jsx'
-import BotPage from '../pages/MainPage/BotPage.jsx'
-import GamePage from '../pages/MainPage/GamePage.jsx'
-import CoursePage from '../pages/MainPage/CoursePage.jsx'
-import QuizPage from '../pages/MainPage/QuizPage.jsx'
-import NotificationPage from '../pages/MainPage/NotificationPage.jsx'
-import { Navigate } from 'react-router-dom'
-import { loginContext } from '../context/context.jsx'
+import LandingPage from '../pages/LandingPage/LandingPage.jsx';
+import MainPage from '../pages/MainPage/MainPage.jsx';
+import BotPage from '../pages/MainPage/BotPage.jsx';
+import GamePage from '../pages/MainPage/GamePage.jsx';
+import CoursePage from '../pages/MainPage/CoursePage.jsx';
+import QuizPage from '../pages/MainPage/QuizPage.jsx';
+import NotificationPage from '../pages/MainPage/NotificationPage.jsx';
 import Login from '../pages/LoginPage/Login.jsx';
 import Signup from '../pages/SignUp/Signup.jsx';
+
 const Routers = () => {
-  const {isLoggedIn,setIsLoggedIn}=useContext(loginContext)
-return(
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
 
-      <Routes>
-        <Route path='/' element={
-          isLoggedIn ? (
-            <Navigate to="/home" />
-          ) : (
-            <LandingPage onLogin={() => setIsLoggedIn(true)} />
-           
-          )
-        }
-      /> 
-      <Route path="/login" element={<Login/>} /> 
-      <Route path="/register" element={<Signup/>} />
-       <Route
-        path="/home"
-        element={isLoggedIn ? <MainPage/> : <Navigate to="/" />}
-      />
-                
-        <Route path='home/notification' element={<NotificationPage />}/>
-        <Route path='home/course' element={<CoursePage />} />
-        <Route path='home/bot' element={<BotPage/>} />
-        <Route path='home/games' element={<GamePage />} />
-        <Route path='home/quiz' element={<QuizPage />} />
-      </Routes>
+      {/* Main Routes */}
+      <Route path="/home" element={<MainPage />} />
+      <Route path="/home/notification" element={<NotificationPage />} />
+      <Route path="/home/course" element={<CoursePage />} />
+      <Route path="/home/bot" element={<BotPage />} />
+      <Route path="/home/games" element={<GamePage />} />
+      <Route path="/home/quiz" element={<QuizPage />} />
+    </Routes>
+  );
+};
 
-    
-  )
-}
-
-export default Routers
-
+export default Routers;
